d095846e026316c74e07e1c293f2cb35ce708318
Amrendra Kumar <amarmca51@gmail.com>
2024-02-20 22:09:03 +0530

    Update freducefunction.js

--- freducefunction.js
+++ freducefunction.js
@@ -8,3 +8,23 @@ const removeDuplicate = items.reduce((acc, item) => {
 }, []);
 
 console.log(removeDuplicate);
+
+...................................
+
+const items = [
+  { name: 'Apple', category: 'Fruit' },
+  { name: 'Onion', category: 'Vegetable' },
+  { name: 'Orange', category: 'Fruit' },
+  { name: 'Lettuce', category: 'Vegetable' },
+];
+
+const groupedItem = items.reduce((acc, item) => {
+  const category = item.category;
+  if (!acc[category]) {
+    acc[category] = [];
+  }
+  acc[category].push(item.name);
+  return acc;
+}, {});
+
+console.log(groupedItem);
